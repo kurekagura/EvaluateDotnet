56c8278f9b069ded8797b423e54c55818ce048b7
kurekagura <26186258+kurekagura@users.noreply.github.com>
2023-10-02 21:39:17 +0900

    Microsoft.Windows.CsWin32の評価コード追加。 ネットワークドライブのUNCを取得するサンプルコード。

---
+++ Microsoft.Windows.CsWin32/HowToGetNetworkDriveInfo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HowToGetNetworkDriveInfo;
+
+/// <summary>
+/// ネットワークドライブのUNC取得はWMIまたはWin32APIを利用する必要がある。
+/// </summary>
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var allDrives = DriveInfo.GetDrives();
+
+        Console.WriteLine("ネットワークドライブ一覧:");
+        foreach (DriveInfo drive in allDrives)
+        {
+            if (drive.DriveType == DriveType.Network)
+            {
+                Console.WriteLine($"ドライブ : {drive.Name}");
+                var driveLetter = drive.Name.Trim('\\'); //X:\の\は渡してはNG。
+                if (Win32Natives.TryWNetGetConnection(driveLetter, out string? unc))
+                {
+                    Console.WriteLine($"  UNC : {unc}");
+                }
+            }
+        }
+    }
+}
---
+++ Microsoft.Windows.CsWin32/HowToGetNetworkDriveInfo/Win32Natives.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HowToGetNetworkDriveInfo;
+
+public static class Win32Natives
+{
+    public static unsafe bool TryWNetGetConnection(string driveLetter, [MaybeNullWhen(false)] out string result, int bufferSize = 1024)
+    {
+        // CS8625 - Cannot convert null literal to non-nullable reference type.
+        result = default;
+        try
+        {
+            // pointer to a zero-terminated string
+            Span<char> pszRemoteName = stackalloc char[bufferSize];
+
+            // pointer to count of characters
+            uint pcchLength = (uint)pszRemoteName.Length;
+
+            fixed (char* pszOutLocal = pszRemoteName)
+            {
+                var ret = Windows.Win32.PInvoke.WNetGetConnection(driveLetter, new Windows.Win32.Foundation.PWSTR(pszOutLocal), ref pcchLength);
+                if (ret != 0)
+                    return false;
+
+                // 確保領域の末尾までが\0で埋められているので削除。
+                result = new string(pszRemoteName.TrimEnd('\0'));
+                return true;
+            }
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
